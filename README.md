eefb601219f9384997582fa81366e45e3c7668ad
Shahab <mokhtary.shahab@gmail.com>
2022-10-10 21:58:16 +0330

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Funding smart contract application which would allow the user to fund the contract for at least $50
+
--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -19,9 +19,9 @@ def test_can_fund_and_withdraw():
 def test_only_owner_can_withdraw():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local testing")
-        account = get_account()
-        fund_me = deploy_fund_me()
-        bad_actor = accounts.add()
+    account = get_account()
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    fund_me.withdraw({"from": bad_actor})
+    with pytest.raises(exceptions.VirtualMachineError):
         fund_me.withdraw({"from": bad_actor})
-        with pytest.raises(exceptions.VirtualMachineError):
-            fund_me.withdraw({"from": bad_actor})
